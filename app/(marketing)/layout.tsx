7677895cbc2d6d26b8e62bcf7f2092a19ae8f43c
Durvesh Bhalekar <durvesh.5.db@gmail.com>
2023-10-30 16:52:10 +0530

    show dashboard button logged in users

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,23 +2,29 @@ import Footer from "@/components/footer";
 import MainNav from "@/components/main-nav";
 import { buttonVariants } from "@/components/ui/button";
 import { marketingConfig } from "@/config/marketing";
+import { getCurrentUser } from "@/lib/session";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getCurrentUser();
   return (
     <div className="min-h-screen">
       <div className="sticky top-0">
         <MainNav items={marketingConfig.mainNav} />
         <Link
-          href="/login"
+          href={user ? "/dashboard" : "/login"}
           className={cn(
             buttonVariants({ variant: "secondary", size: "lg" }),
             "absolute right-10 top-4"
           )}
         >
-          Login
+          {user ? "Dashboard" : "Login"}
         </Link>
       </div>
       {children}
